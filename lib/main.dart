3547a8cc3ee2ae4c8743b05f47b28b805ccf20e5
PedRaM <pedramXIII@gmail.com>
2024-04-28 12:33:07 +0330

    fix some issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,22 @@
-import 'dart:async';
-import 'dart:math';
+import 'dart:io';
 
-coolPrint(
-  String message,
-  int animationDuration,
-  Function() onFinish,
-  String? header,
-) async {
-  String temp = '';
-  int i = 0;
-  bool breaker() => temp == message;
-  _findLetter(message[i], animationDuration, breaker, onFinish)
-      .listen((event) async {
-    final isValid = temp.isEmpty
-        ? message[i] == event
-        : temp[temp.length - 1] == message[i];
-    temp = isValid
-        ? '$temp$event'
-        : '${temp.substring(0, max(temp.length - 1, 0))}$event';
-    _printMessage(temp, header);
-    if (isValid) i++;
-  });
+import 'cool_print.dart';
+
+void main(List<String> arguments) {
+  print('Enter your custom message');
+  final message = stdin.readLineSync();
+  print('enter your custom animation duration');
+  final animationDuration = int.tryParse(stdin.readLineSync() ?? '');
+  final header = animationDuration == null
+      ? 'duration is not valid. so i use default 25 millseconds value :)'
+      : null;
+  if (header != null) print('header');
+  CoolPrinter.coolPrint(
+    message ?? 'Null Value',
+    animationDuration ?? 25,
+    () {
+      print('Finished :). Thanks for using this application.');
+    },
+    header,
+  );
 }
-
-Stream<String> _findLetter(
-  String letter,
-  int duration,
-  bool Function() breaker,
-  Function() onFinish,
-) async* {
-  int i = 'A'.codeUnitAt(0);
-  while (true) {
-    final result = String.fromCharCode(i);
-    yield result;
-    if (i == 'Z'.codeUnitAt(0)) {
-      i = 'a'.codeUnitAt(0);
-    } else if (i == 'z'.codeUnitAt(0)) {
-      i = ' '.codeUnitAt(0);
-    } else if (i == ' '.codeUnitAt(0)) {
-      i = 'A'.codeUnitAt(0);
-    } else {
-      i++;
-    }
-    await _delay(duration);
-    if (breaker()) {
-      onFinish();
-      break;
-    }
-  }
-}
-
-void _printMessage(String message, String? header) {
-  _clearConsole();
-  if (header != null) print(header);
-  print(message);
-}
-
-void _clearConsole() => print('\x1B[2J\x1B[0;0H');
-
-Future<void> _delay(int duration) =>
-    Future.delayed(Duration(milliseconds: duration));
